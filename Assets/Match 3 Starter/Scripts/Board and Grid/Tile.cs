103eda2fdb0fb2ec4e9666c1866f015062831812
Kirill Bel <kirillka-bel@mail.ru>
2024-06-14 01:32:39 +0300

    fix swap2

--- Assets/Match 3 Starter/Scripts/Board and Grid/Tile.cs
+++ Assets/Match 3 Starter/Scripts/Board and Grid/Tile.cs
@@ -1,44 +1,47 @@
-﻿
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class Tile : MonoBehaviour {
-	private static Color selectedColor = new Color(.5f, .5f, .5f, 1.0f);
-	private static Tile previousSelected = null;
+public class Tile : MonoBehaviour
+{
+    private static Color selectedColor = new Color(.5f, .5f, .5f, 1.0f);
+    private static Tile previousSelected = null;
 
-	private SpriteRenderer render;
-	private bool isSelected = false;
+    private SpriteRenderer render;
+    private bool isSelected = false;
 
-	private Vector2[] adjacentDirections = new Vector2[] { Vector2.up, Vector2.down, Vector2.left, Vector2.right };
+    private Vector2[] adjacentDirections = new Vector2[] { Vector2.up, Vector2.down, Vector2.left, Vector2.right };
     private bool matchFound = false;
 
-    void Awake() {
-		render = GetComponent<SpriteRenderer>();
+    void Awake()
+    {
+        render = GetComponent<SpriteRenderer>();
     }
 
-	private void Select() {
-		isSelected = true;
-		render.color = selectedColor;
-		previousSelected = gameObject.GetComponent<Tile>();
-		SFXManager.instance.PlaySFX(Clip.Select);
-	}
+    private void Select()
+    {
+        isSelected = true;
+        render.color = selectedColor;
+        previousSelected = gameObject.GetComponent<Tile>();
+        SFXManager.instance.PlaySFX(Clip.Select);
+    }
 
-	private void Deselect() {
-		isSelected = false;
-		render.color = Color.white;
-		previousSelected = null;
-	}
+    private void Deselect()
+    {
+        isSelected = false;
+        render.color = Color.white;
+        previousSelected = null;
+    }
+
     void OnMouseDown()
     {
-        
         if (render.sprite == null || BoardManager.instance.IsShifting)
         {
             return;
         }
 
         if (isSelected)
-        { //  Is it already selected?
+        { // Is it already selected?
             Deselect();
         }
         else
@@ -50,32 +53,47 @@ public class Tile : MonoBehaviour {
             else
             {
                 if (GetAllAdjacentTiles().Contains(previousSelected.gameObject))
-                { 
-                    SwapSprite(previousSelected.render);
-                    previousSelected.ClearAllMatches();
-                    previousSelected.Deselect();
-                    ClearAllMatches();
+                {
+                    StartCoroutine(TryMatch(previousSelected));
                 }
                 else
-                { 
-                    previousSelected.GetComponent<Tile>().Deselect();
+                {
+                    previousSelected.Deselect();
                     Select();
                 }
             }
         }
     }
+
+    private IEnumerator TryMatch(Tile otherTile)
+    {
+        SwapSprite(otherTile.render);
+        yield return new WaitForSeconds(0.5f); // Small delay to show the swap
+
+        if (!HasMatch() && !otherTile.HasMatch())
+        {
+            // If no matches, swap back
+            SwapSprite(otherTile.render);
+        }
+
+        otherTile.ClearAllMatches();
+        otherTile.Deselect();
+        ClearAllMatches();
+    }
+
     public void SwapSprite(SpriteRenderer render2)
-    { 
+    {
         if (render.sprite == render2.sprite)
-        { 
+        {
             return;
         }
 
-        Sprite tempSprite = render2.sprite; 
-        render2.sprite = render.sprite; 
-        render.sprite = tempSprite; 
-        SFXManager.instance.PlaySFX(Clip.Swap); 
+        Sprite tempSprite = render2.sprite;
+        render2.sprite = render.sprite;
+        render.sprite = tempSprite;
+        SFXManager.instance.PlaySFX(Clip.Swap);
     }
+
     private GameObject GetAdjacent(Vector2 castDir)
     {
         RaycastHit2D hit = Physics2D.Raycast(transform.position, castDir);
@@ -85,6 +103,7 @@ public class Tile : MonoBehaviour {
         }
         return null;
     }
+
     private List<GameObject> GetAllAdjacentTiles()
     {
         List<GameObject> adjacentTiles = new List<GameObject>();
@@ -94,6 +113,7 @@ public class Tile : MonoBehaviour {
         }
         return adjacentTiles;
     }
+
     private List<GameObject> FindMatch(Vector2 castDir)
     {
         List<GameObject> matchingTiles = new List<GameObject>();
@@ -105,22 +125,24 @@ public class Tile : MonoBehaviour {
         }
         return matchingTiles;
     }
-    private void ClearMatch(Vector2[] paths) 
+
+    private void ClearMatch(Vector2[] paths)
     {
-        List<GameObject> matchingTiles = new List<GameObject>(); // 2
-        for (int i = 0; i < paths.Length; i++) 
+        List<GameObject> matchingTiles = new List<GameObject>();
+        for (int i = 0; i < paths.Length; i++)
         {
             matchingTiles.AddRange(FindMatch(paths[i]));
         }
-        if (matchingTiles.Count >= 2) 
+        if (matchingTiles.Count >= 2)
         {
-            for (int i = 0; i < matchingTiles.Count; i++) 
+            for (int i = 0; i < matchingTiles.Count; i++)
             {
                 matchingTiles[i].GetComponent<SpriteRenderer>().sprite = null;
             }
-            matchFound = true; 
+            matchFound = true;
         }
     }
+
     public void ClearAllMatches()
     {
         if (render.sprite == null)
@@ -138,4 +160,16 @@ public class Tile : MonoBehaviour {
         }
     }
 
+    private bool HasMatch()
+    {
+        if (render.sprite == null)
+            return false;
+
+        List<GameObject> horizontalMatch = FindMatch(Vector2.left);
+        horizontalMatch.AddRange(FindMatch(Vector2.right));
+        List<GameObject> verticalMatch = FindMatch(Vector2.up);
+        verticalMatch.AddRange(FindMatch(Vector2.down));
+
+        return horizontalMatch.Count >= 2 || verticalMatch.Count >= 2;
+    }
 }
