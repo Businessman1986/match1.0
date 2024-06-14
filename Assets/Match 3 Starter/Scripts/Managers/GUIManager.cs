92b5f2d2a3d7c8208e994680f28f6c2bfacce438
Kirill Bel <kirillka-bel@mail.ru>
2024-06-14 05:42:31 +0300

    Records-part1

--- Assets/Match 3 Starter/Scripts/Managers/GUIManager.cs
+++ Assets/Match 3 Starter/Scripts/Managers/GUIManager.cs
@@ -1,74 +1,87 @@
-﻿
-
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 
-public class GUIManager : MonoBehaviour {
-	public static GUIManager instance;
+public class GUIManager : MonoBehaviour
+{
+    public static GUIManager instance;
 
-	public GameObject gameOverPanel;
-	public Text yourScoreTxt;
-	public Text highScoreTxt;
+    public GameObject gameOverPanel;
+    public Text yourScoreTxt;
+    public Text highScoreTxt;
+    public Text scoreTxt;
+    public Text moveCounterTxt;
+    public RecordsManager recordsManager;  // Добавьте ссылку на RecordsManager
 
-	public Text scoreTxt;
-	public Text moveCounterTxt;
+    private int score;
+    private float gameDuration = 60f;  // Длительность игры в секундах
+    private float timeRemaining;
 
-	private int score;
-	private float gameDuration = 60f;  // Duration of the game in seconds
-	private float timeRemaining;
-	public int Score
-	{
-		get
-		{
-			return score;
-		}
+    public int Score
+    {
+        get
+        {
+            return score;
+        }
+        set
+        {
+            score = value;
+            scoreTxt.text = score.ToString();
+        }
+    }
 
-		set
-		{
-			score = value;
-			scoreTxt.text = score.ToString();
-		}
-	}
-	void Awake() {
-		instance = this;
-		timeRemaining = gameDuration;
-	}
-	void Start()
-	{
-		StartCoroutine(GameTimer());
-	}
-	IEnumerator GameTimer()
-	{
-		while (timeRemaining > 0)
-		{
-			yield return new WaitForSeconds(1f);
-			timeRemaining--;
-			UpdateTimerText();
-		}
-		GameOver();
-	}
-	void UpdateTimerText()
-	{
-		int minutes = Mathf.FloorToInt(timeRemaining / 60F);
-		int seconds = Mathf.FloorToInt(timeRemaining % 60F);
-		moveCounterTxt.text = string.Format("{0:0}:{1:00}", minutes, seconds);
-	}
+    void Awake()
+    {
+        instance = this;
+        timeRemaining = gameDuration;
+    }
 
-	// Show the game over panel
-	public void GameOver() {
-		GameManager.instance.gameOver = true;
+    void Start()
+    {
+        StartCoroutine(GameTimer());
+    }
 
-		gameOverPanel.SetActive(true);
+    IEnumerator GameTimer()
+    {
+        while (timeRemaining > 0)
+        {
+            yield return new WaitForSeconds(1f);
+            timeRemaining--;
+            UpdateTimerText();
+        }
+        GameOver();
+    }
 
-		if (score > PlayerPrefs.GetInt("HighScore")) {
-			PlayerPrefs.SetInt("HighScore", score);
-			highScoreTxt.text = "New Best: " + PlayerPrefs.GetInt("HighScore").ToString();
-		} else {
-			highScoreTxt.text = "Best: " + PlayerPrefs.GetInt("HighScore").ToString();
-		}
+    void UpdateTimerText()
+    {
+        int minutes = Mathf.FloorToInt(timeRemaining / 60F);
+        int seconds = Mathf.FloorToInt(timeRemaining % 60F);
+        moveCounterTxt.text = string.Format("{0:0}:{1:00}", minutes, seconds);
+    }
 
-		yourScoreTxt.text = score.ToString();
-	}
+    // Show the game over panel
+    public void GameOver()
+    {
+        GameManager.instance.gameOver = true;
 
+        gameOverPanel.SetActive(true);
+
+        if (score > PlayerPrefs.GetInt("HighScore"))
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+            highScoreTxt.text = "New Best: " + PlayerPrefs.GetInt("HighScore").ToString();
+        }
+        else
+        {
+            highScoreTxt.text = "Best: " + PlayerPrefs.GetInt("HighScore").ToString();
+        }
+
+        yourScoreTxt.text = score.ToString();
+
+        // Добавить текущий счёт в рекорды
+        if (recordsManager != null)
+        {
+            recordsManager.AddScore(score);
+        }
+    }
 }
---
+++ Assets/Match 3 Starter/Scripts/Managers/RecordsManager.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class RecordsManager : MonoBehaviour
+{
+    public GameObject recordsPanel;
+    public Text recordsText;
+
+    private List<int> scores;
+
+    void Start()
+    {
+        LoadScores();
+        DisplayScores();
+    }
+
+    public void AddScore(int score)
+    {
+        scores.Add(score);
+        scores.Sort((a, b) => b.CompareTo(a)); // Сортировка по убыванию
+
+        // Оставить только топ-10 результатов
+        if (scores.Count > 10)
+        {
+            scores.RemoveAt(scores.Count - 1);
+        }
+
+        SaveScores();
+        DisplayScores();
+    }
+
+    private void LoadScores()
+    {
+        scores = new List<int>();
+
+        for (int i = 0; i < 10; i++)
+        {
+            if (PlayerPrefs.HasKey("Score" + i))
+            {
+                scores.Add(PlayerPrefs.GetInt("Score" + i));
+            }
+        }
+    }
+
+    private void SaveScores()
+    {
+        for (int i = 0; i < scores.Count; i++)
+        {
+            PlayerPrefs.SetInt("Score" + i, scores[i]);
+        }
+    }
+
+    private void DisplayScores()
+    {
+        recordsText.text = "Records:\n";
+        for (int i = 0; i < scores.Count; i++)
+        {
+            recordsText.text += (i + 1) + ". " + scores[i] + "\n";
+        }
+    }
+
+    // Метод должен быть публичным, чтобы появляться в инспекторе
+    public void ShowRecords()
+    {
+        recordsPanel.SetActive(true);
+    }
+
+    public void HideRecords()
+    {
+        recordsPanel.SetActive(false);
+    }
+}
